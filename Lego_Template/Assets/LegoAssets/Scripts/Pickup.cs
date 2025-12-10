0b278d399f64df1b86fb93e232afe375625657ff
Nattawut-artn <natthawutartnonladev@gmail.com>
2025-12-10 22:19:44 +0700

    chore : import asset and move gitignore

---
+++ Lego_Template/Assets/LegoAssets/Scripts/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class Pickup : MonoBehaviour {
+
+	//public GameObject MyPrefab;
+	//GameObject myPrefab;
+
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			ScoreController.score += 10;
+			ScoreController.currentGold ++;
+			//myPrefab = Instantiate(MyPrefab, this.transform.position, MyPrefab.transform.rotation) as GameObject;
+			gameObject.SetActive(false);
+		}
+	}
+}
